bb08609b6f3acd62dce8c73ea84257fccbf43471
julius-novachrono <mohanrajd881@gmail.com>
2026-03-27 20:33:50 +0530

    updated

--- app.py
+++ app.py
@@ -405,4 +405,4 @@ if __name__ == "__main__":
     print("  Logs          -> http://localhost:5000/logs")
     print("  Registrations -> http://localhost:5000/registrations")
     print("=" * 50)
-    app.run(debug=True, port=5000)
+    app.run(debug=True,host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
